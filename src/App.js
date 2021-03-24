a1ef5c3eada6642ff78b49db23798d0c5b9c2193
HonvedoAdam <honvedoadamkj@gmail.com>
2021-03-24 21:26:39 +0100

    create footer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-
 import "./App.scss";
-import Navbar from "./components/Navbar"
-import FirstScreen from "./components/FirstScreen"
-import SecondScreen from "./components/SecondScreen"
+import Navbar from "./components/Navbar";
+import FirstScreen from "./components/FirstScreen";
+import SecondScreen from "./components/SecondScreen";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">
-    <Navbar />
-    <FirstScreen />
-    <SecondScreen />
-  </div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <FirstScreen />
+      <SecondScreen />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
